9b5106a221cbfee7c187f5bb2e453c0fa6ade636
Shrek <slavadfg@mail.ru>
2023-12-17 21:57:39 +0300

    Added files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/User/Downloads/code/Task_6
+# Build directory: D:/User/Downloads/code/Task_6/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
